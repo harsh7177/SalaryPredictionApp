9d746c2284cb74ccb9c88e5c6eec1241c1985870
Harsh Kandari <harshsingh7177@gmail.com>
2024-04-17 16:43:54 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from predict_page import show_predict_page
+from explore_page import show_explore_page
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
+import pickle
+
+
+page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
+
+if page == "Predict":
+    show_predict_page()
+else:
+    show_explore_page()
